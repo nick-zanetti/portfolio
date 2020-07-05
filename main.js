20c09207c8853b2a830666252dac1dc1693af300
Nick Zanetti <nzanetti44@gmail.com>
2020-07-05 16:51:20 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const backToTopButton = document.querySelector("#back-to-top")
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) {
+    backToTopButton.style.display = "block"
+  }
+  else {
+    backToTopButton.style.display = "none";
+  }
+}
+
+backToTopButton.addEventListener("click", backToTop)
+
+function backToTop() {
+  window.scrollTo(0, 0);
+}
+
